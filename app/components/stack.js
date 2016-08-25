79a839e600648ffe871c5eccb8b56c54c29bf586
Michelle Nguyen <ngynmt@gmail.com>
2016-08-25 14:53:02 -0700

    fix/minor style changes

--- app/components/stack.js
+++ app/components/stack.js
@@ -5,6 +5,7 @@ let Stack = () => {
     <div className='details'>
       <h1>technologies used</h1>
       <p>This iOS application was built in React-Native, Three.js, Node, Express, and Redis.</p>
+      <p>View the repository <a href='https://github.com/CalculatingPineapples/neARby'>here</a>.</p>
     </div>
   );
 }
