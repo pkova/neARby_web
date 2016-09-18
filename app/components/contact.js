3d395a3d41bacb6269506fd0d1a40e701755e1fe
Peter Kovanen <pkova@users.noreply.github.com>
2016-09-17 22:21:56 -0700

    Fix contact information

--- app/components/contact.js
+++ app/components/contact.js
@@ -6,7 +6,7 @@ let Contact = () => {
       <h1>contact</h1>
       <p>Robb Marsee • <a href='http://github.com/ro-double-b'>github</a> • <a href='https://www.linkedin.com/in/robb-marsee'>linkedin</a></p>
       <p>Ling Chen • <a href='http://github.com/lingyc'>github</a> • <a href='https://www.linkedin.com/in/ling-yu-chen-1b967312'>linkedin</a></p>
-      <p>Pyry Kovanen • <a href='http://github.com/pkova'>github</a> • <a href='https://www.linkedin.com/in/pyry-kovanen'>linkedin</a></p>
+      <p>Peter Kovanen • <a href='http://github.com/pkova'>github</a> • <a href='https://www.linkedin.com/in/peterkovanen'>linkedin</a></p>
       <p>Michelle Nguyen • <a href='http://github.com/ngynmt'>github</a> • <a href='https://www.linkedin.com/in/nguyenchelle'>linkedin</a></p>
     </div>
   );
